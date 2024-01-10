6d2f24420acc489946fb0b82311b5d18713250d4
n-haruhi <hr23nakanishi@gmail.com>
2024-01-10 09:57:53 +0000

    [Update]ルーティングミス修正

--- config/routes.rb
+++ config/routes.rb
@@ -23,14 +23,8 @@ Rails.application.routes.draw do
     resources :articles, only: [:index, :show]
   end
 
-  devise_for :public,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-  }
-
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-  }
+  devise_for :user
+  devise_for :admin
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
