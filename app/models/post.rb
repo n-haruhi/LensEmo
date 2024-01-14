106a86a40e1f40569c1b11fe290c02bfa4118b11
n-haruhi <hr23nakanishi@gmail.com>
2024-01-14 13:58:39 +0000

    [Update]いいね機能モデル修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,12 +6,18 @@ class Post < ApplicationRecord
   has_many :post_tag_relations, dependent: :destroy
   # postsテーブルから中間テーブルを介してtagsテーブルへ
   has_many :tags, through: :post_tag_relations, dependent: :destroy
-
+  # 通知機能: ポリモーフィック関連
+  # Notificationモデルをnotifiableという名前で扱う
   has_many :notifications, as: :notifiable, dependent: :destroy
 
   # Active Storageの設定。投稿画像と気持ちを表す画像を用いる。
   has_one_attached :post_image
   has_one_attached :emotion_image
+
+  # 引数で渡されたユーザidがFavoritesテーブル内に存在するかどうかを調べtrueかfalseを返す
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 
   # データベースへの保存処理の実行後に処理実行
   # Postモデルのレコード作成に合わせてPost投稿者のフォロワーを取得し、
