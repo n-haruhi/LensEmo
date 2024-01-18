52d4369b560b2977a2c22a7f1a59299e29e3b881
n-haruhi <hr23nakanishi@gmail.com>
2024-01-18 12:57:38 +0000

    [Update]タグ検索機能バグ修正(済)

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,5 +3,5 @@ class Tag < ApplicationRecord
   # tagsテーブルから中間テーブルに対して
   has_many :post_tag_relations, dependent: :destroy
   # tagsテーブルから中間テーブルを介してpostsテーブルへ
-  has_many :tags, through: :post_tag_relations, dependent: :destroy
+  has_many :posts, through: :post_tag_relations, dependent: :destroy
 end
