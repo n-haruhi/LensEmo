411e099d79dedf6071279338fa53e90cc44d5f30
n-haruhi <hr23nakanishi@gmail.com>
2024-01-27 19:05:52 +0000

    [Update]tidying up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,12 @@
 class ApplicationController < ActionController::Base
-  # /adminを含むパスじゃなかったらユーザー側ログイン認証。topとaboutは除外
-  before_action :authenticate_user!, except: [:top, :about]#, unless: :admin_url
-  # /adminを含むパスなら、管理者側ログイン認証。
-  #before_action :authenticate_admin!, if: :admin_url
+  # ユーザーログイン認証。topとaboutは除外
+  before_action :authenticate_user!, except: [:top, :about]
   # deviseを利用する機能(ユーザー登録やログイン認証等)が行われる前にメソッド実行
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   # protectedは呼び出された他のコントローラからも参照することができる
   protected
-  # パスに「/admin」が含まれているか確認。
-  # request.fullpathで飛んだ先のpathを全て取得。include?(/admin)で/adminと同じ要素が含まれていれば、trueを返す。
-  #def admin_url
-  #  request.fullpath.include?("/admin")
-  #end
 
   # ユーザー登録のときにユーザー名のデータ操作を許可する
   def configure_permitted_parameters
